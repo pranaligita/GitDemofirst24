820971f34e522f74397123a68e65253061bf44b2
pranaligita <ishaansawant8@gmail.com>
2024-04-02 07:59:38 +0530

    Added Calculator

--- Gitproject/src/Calculator.java
+++ Gitproject/src/Calculator.java
@@ -3,5 +3,8 @@ public class Calculator {
     public static void main(String[]args)
     {
         System.out.println("Hello");
+        System.out.println("pranali");
+        System.out.println("rane");
+
     }
 }
